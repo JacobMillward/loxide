6b8d392c55a5848f5c5ff9cb44b444991b662db5
Jacob Millward <jacob@millward.dev>
2022-03-12 16:38:45 +0000

    Add basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
+fn print_help(err: Option<String>) {
+    if let Some(msg) = err {
+        println!("{}", msg)
+    }
+    println!("usage: loxide <string>
+    Interprets the string as lox, and runs the resultant program
+");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => print_help(None),
+        2 => println!("Run"),
+        _ => print_help(Some(format!("Incorrect number of arguments")))
+    }
 }
