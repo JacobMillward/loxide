13c4eb8e07ae1ab8727e11190d29233ee9fadee2
Jacob Millward <jacob@millward.dev>
2022-03-12 19:41:28 +0000

    Implement rough structure of parsing a file with errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+use core::fmt;
+use std::{error, fs, io};
+
+#[derive(Debug)]
+pub struct LoxErrorReport {
+    line_number: usize,
+    location: String,
+    message: String,
+}
+
+impl fmt::Display for LoxErrorReport {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Line: {}, Loc: {}, Message: {}",
+            self.line_number, self.location, self.message,
+        )
+    }
+}
+
+#[derive(Debug)]
+pub enum LoxScriptError {
+    IoError(io::Error),
+    LoxError(LoxErrorReport),
+}
+
+impl error::Error for LoxScriptError {}
+
+impl fmt::Display for LoxScriptError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IoError(e) => e.fmt(f),
+            Self::LoxError(r) => r.fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for LoxScriptError {
+    fn from(err: io::Error) -> Self {
+        LoxScriptError::IoError(err)
+    }
+}
+
+impl From<LoxErrorReport> for LoxScriptError {
+    fn from(err: LoxErrorReport) -> Self {
+        LoxScriptError::LoxError(err)
+    }
+}
+pub fn run_file(file_path: &str) -> Result<(), LoxScriptError> {
+    let input = fs::read_to_string(file_path)?;
+    run(&input)?;
+    Ok(())
+}
+
+pub fn run(lox_str: &str) -> Result<(), LoxErrorReport> {
+    for token in lox_str.split_whitespace() {
+        println!("{}", token);
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
+use loxide::run_file;
 use std::env;
 
-fn print_help(err: Option<String>) {
-    if let Some(msg) = err {
-        println!("{}", msg)
-    }
+fn print_help() {
     println!(
-        "usage: loxide <string>
-    Interprets the string as lox, and runs the resultant program"
+        "usage: loxide <script>
+    Interprets and runs the passed path as a lox script"
     );
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        1 => print_help(None),
-        2 => println!("Run"),
-        _ => print_help(Some(format!("Incorrect number of arguments"))),
+        1 => {
+            print_help();
+            Err("No path specified.")?
+        }
+        2 => Ok(run_file(&args[1])?),
+        _ => {
+            print_help();
+            Err("Incorrect number of arguments.")?
+        }
     }
 }
