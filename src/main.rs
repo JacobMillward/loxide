b577064b15ca9ebcb8a8824afcbc77de40a76a5e
Jacob Millward <jacob@millward.dev>
2022-03-12 16:47:58 +0000

    Format `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ fn print_help(err: Option<String>) {
     if let Some(msg) = err {
         println!("{}", msg)
     }
-    println!("usage: loxide <string>
-    Interprets the string as lox, and runs the resultant program
-");
+    println!(
+        "usage: loxide <string>
+    Interprets the string as lox, and runs the resultant program"
+    );
 }
 
 fn main() {
@@ -15,6 +16,6 @@ fn main() {
     match args.len() {
         1 => print_help(None),
         2 => println!("Run"),
-        _ => print_help(Some(format!("Incorrect number of arguments")))
+        _ => print_help(Some(format!("Incorrect number of arguments"))),
     }
 }
